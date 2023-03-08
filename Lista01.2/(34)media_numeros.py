87c4a7fe16b09317fd37861e4003879ee78a3629
Ryan <ryanfaustinocarvalho@gmail.com>
2023-03-08 01:31:47 -0300

    Upload

--- Lista01.2/(34)media_numeros.py
+++ Lista01.2/(34)media_numeros.py
@@ -1,7 +1,10 @@
+# Entrada
 num1 = float(input('Digite o primeiro número: '))
 num2 = float(input('Digite o segundo número: '))
 num3 = float(input('Digite o terceiro número: '))
 
+# Processamento:
 avr = (num1 + num2 + num3) / 3
 
-print(f'A média dos números {num1}, {num2} e {num3} é {avr:.2f} \n Sua média inteira é {int(avr)}')
+# Saída:
+print(f'A média dos números {num1}, {num2} e {num3} é {avr:.2f} \nSua média inteira é {int(avr)}')
